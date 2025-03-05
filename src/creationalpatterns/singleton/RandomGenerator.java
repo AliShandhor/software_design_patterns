de04d6459f18936ee380e2f2323b560cc6063548
Ali Shandhor <132630605+AliShandhor@users.noreply.github.com>
2025-03-05 03:57:54 -0500

    Update RandomGenerator.java

--- src/creationalpatterns/singleton/RandomGenerator.java
+++ src/creationalpatterns/singleton/RandomGenerator.java
@@ -1,14 +1,22 @@
-package creationalpatterns.singleton;
-import java.util.Random;
 
-// We follow rules of singleton pattern rules + adding lazy initliazation to provide creating multiple objects for multithreading scenarios
+package creationalpatterns.singleton;
+    // The RandomGenerator class demonestartes the Singleton pattern with lazy initialization
+    // and thread safety. It ensures a single instance is created, even in multi-threaded environments,
+    // using double-checked locking. The getRandomGeneratorInstance() method creates the instance 
+    // only when needed. The generateRandomNumbers() method generates a random number between 0 and 99.
+    // The private constructor prevents external instantiation, enforcing the Singleton pattern.   
+
+import java.util.Random;
+// We follow rules of singleton pattern rules - mentioned in SingletonObject.java + adding lazy initliazation to provide creating multiple objects for multithreading scenarios
 public class RandomGenerator {
     private static  RandomGenerator randomGeneratorInstance;
-
+    // Private constructor prevents external instantiation
     private RandomGenerator(){};
 
     public static RandomGenerator getRandomGeneratorInstance(){
+        // Eensures one instrance will only be initialized 
         if (randomGeneratorInstance == null){
+            // Lazy initliazation for multie-threading purpose
             synchronized (RandomGenerator.class){
                 if (randomGeneratorInstance == null) {
                     randomGeneratorInstance = new RandomGenerator();
